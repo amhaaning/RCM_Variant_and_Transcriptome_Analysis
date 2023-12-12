8900ee9fda510c9b9e3579cf2f117f83d9ca9a8d
Allison H <33882083+amhaaning@users.noreply.github.com>
2023-12-12 14:53:01 -0600

    Update GenotypeGVCFs.sh

--- GenotypeGVCFs.sh
+++ GenotypeGVCFs.sh
@@ -7,15 +7,15 @@
 #SBATCH --mail-user=ahaaning@iu.edu
 #SBATCH --mail-type=ALL
 
-genomicsdb=/N/project/WareLab_ARP_NGS/ILMN_249_Ware_WESseq18_Apr2018_GVCF/gatk_genomicsdb/
+genomicsdb=./GVCF/gatk_genomicsdb/
 
 #Output directory
-outdir=/N/project/WareLab_ARP_NGS/RCM_VCFs/
+outdir=./RCM_VCFs/
 
 #Create vcf output file name
 outfile=$outdir"RCM_WES_Apr18_Joint.vcf.gz"
 
 #Reference genome
-Ref=/N/project/Ware-lab_NGS/References/gatk_resource_bundle/Homo_sapiens_assembly38.fasta
+Ref=./References/gatk_resource_bundle/Homo_sapiens_assembly38.fasta
 
 gatk --java-options "-Xms35g -Xmx35g -XX:ParallelGCThreads=4" GenotypeGVCFs -R $Ref -V gendb://$genomicsdb -O $outfile --allow-old-rms-mapping-quality-annotation-data true
